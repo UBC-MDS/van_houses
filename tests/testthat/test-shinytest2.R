4eb465223ac1c8d7bfe92e7b482e4df15a5a372d
hcwang24 <hcwang24@student.ubc.ca>
2023-03-17 02:36:27 -0700

    testing_sliders

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -5,3 +5,14 @@ test_that("{shinytest2} recording: launch_test", {
   app$set_inputs(DataTables_Table_0_length = "10")
   app$expect_values(output = "avg_price")
 })
+
+
+test_that("{shinytest2} recording: slider_button_test", {
+  app <- AppDriver$new(name = "slider_button_test", height = 736, width = 1169)
+  app$set_inputs(DataTables_Table_0_length = "10")
+  app$set_inputs(reportyear = "2020")
+  app$set_inputs(priceslider = c(3e+05, 3760000))
+  app$set_inputs(yearslider = c(1975, 1994))
+  app$expect_values(output = "num_houses")
+  app$expect_values(output = "avg_price")
+})
