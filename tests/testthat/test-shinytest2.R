8e1b66463cae878b631d082d3c0a429a735b66aa
hcwang24 <hcwang24@student.ubc.ca>
2023-03-17 02:40:22 -0700

    selection_box_tests

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -16,3 +16,13 @@ test_that("{shinytest2} recording: slider_button_test", {
   app$expect_values(output = "num_houses")
   app$expect_values(output = "avg_price")
 })
+
+
+test_that("{shinytest2} recording: testing_selection_box", {
+  app <- AppDriver$new(name = "testing_selection_box", height = 736, width = 1169)
+  app$set_inputs(DataTables_Table_0_length = "10")
+  app$set_inputs(select_all_zoning = TRUE)
+  app$set_inputs(community = c("Shaughnessy", "Kerrisdale", "Downtown", "Marpole"))
+  app$expect_values(output = "avg_price")
+  app$expect_values(output = "num_houses")
+})
